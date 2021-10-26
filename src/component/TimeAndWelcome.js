338a1c8dca0672c6469704fe29dce6d98515bf3b
nggoong <nggoong@naver.com>
2021-10-26 14:49:50 +0900

    실시간 시간 갱신기능 구현✔

--- src/component/TimeAndWelcome.js
+++ src/component/TimeAndWelcome.js
@@ -1,9 +1,23 @@
+import React, { useState } from 'react';
+
+
 function TimeAndWelcome () {
+    let [currentTime, setCurrentTime] = useState("");
+
+    function getTime() {
+        let today = new Date();
+        let hour = today.getHours();
+        let min = today.getMinutes();
+        let time = "";
+        time = hour + " : " + min;
+        setCurrentTime(time);
+    }
+    let interval = setInterval(() => { getTime(); }, 1000);
     return(
         <div className = "TimeAndWelcome">
             <div className="TimeAndWelcome_wrapper">
                 <div className = "time">
-                    11:28
+                    {currentTime}
                 </div>
                 <div className="welcome">
                     Good morning, nggoong.
